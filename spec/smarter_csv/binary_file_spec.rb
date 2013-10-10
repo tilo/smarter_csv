c34e549729eacf26064c7d1c3ac3e6643f3dbbf6
Tilo Sloboda <tilo.sloboda@gmail.com>
2013-10-10 00:21:02 -0700

    adding rspec test for binary files

---
+++ spec/smarter_csv/binary_file_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+fixture_path = 'spec/fixtures'
+
+# this reads a binary database dump file, which is in structure like a CSV file
+# but contains control characters delimiting the rows and columns, and also 
+# contains a comment section which is commented our by a leading # character
+
+describe 'be_able_to' do
+  it 'loads_binary_file_with_comments' do 
+    options = {:col_sep => "\cA", :row_sep => "\cB", :comment_regexp => /^#/}
+    data = SmarterCSV.process("#{fixture_path}/binary.csv", options)
+    data.flatten.size.should == 8
+    data.each do |item|
+      item[:timestamp].should == 1381388409
+      item[:item_id].class.should be == Fixnum
+      item[:name].size.should be > 0
+    end
+  end
+end
