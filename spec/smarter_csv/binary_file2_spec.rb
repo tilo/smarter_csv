44075dcfcd0763c99aed9fea5a24b16c67898de7
Tilo Sloboda <tilo.sloboda@gmail.com>
2013-10-10 00:27:59 -0700

    adding rspec tests

--- spec/smarter_csv/binary_file2_spec.rb
+++ spec/smarter_csv/binary_file2_spec.rb
@@ -6,17 +6,19 @@ fixture_path = 'spec/fixtures'
 # but contains control characters delimiting the rows and columns, and also 
 # contains a comment section which is commented our by a leading # character
 
+# same as binary_file_spec , but reading the file with strings as keys
+
 describe 'be_able_to' do
-  it 'loads_binary_file_with_comments' do 
-    options = {:col_sep => "\cA", :row_sep => "\cB", :comment_regexp => /^#/}
+  it 'loads_binary_file_with_strings_as_keys' do 
+    options = {:col_sep => "\cA", :row_sep => "\cB", :comment_regexp => /^#/, :strings_as_keys => true}
     data = SmarterCSV.process("#{fixture_path}/binary.csv", options)
     data.flatten.size.should == 8
     data.each do |item|
-      # all keys should be symbols
-      item.keys.each{|x| x.class.should be == Symbol}
-      item[:timestamp].should == 1381388409
-      item[:item_id].class.should be == Fixnum
-      item[:name].size.should be > 0
+      # all keys should be strings
+      item.keys.each{|x| x.class.should be == String}
+      item['timestamp'].should == 1381388409
+      item['item_id'].class.should be == Fixnum
+      item['name'].size.should be > 0
     end
   end
 end
