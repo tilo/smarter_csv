71344cec27fa53def7d9089135693d228e76947e
Tilo Sloboda <tilo.sloboda@gmail.com>
2013-10-10 17:33:12 -0700

    adding to spec

--- spec/smarter_csv/load_basic_spec.rb
+++ spec/smarter_csv/load_basic_spec.rb
@@ -6,6 +6,8 @@ describe 'be_able_to' do
   it 'loads_basic_csv_file' do 
     data = SmarterCSV.process("#{fixture_path}/basic.csv")
     data.size.should == 4
+    # all the keys should be symbols
+    data.each{|item| item.keys.each{|x| x.class.should be == Symbol}}
     data.each do |h|
       h.size.should <= 6
     end
