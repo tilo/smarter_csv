278c830526e5266ead6b9fa5474625d113897b3f
Hugo Lepetit <giglemad@gmail.com>
2014-01-06 15:58:01 +0100

    Feature convert_values_to_numeric is now tested and gets 'except' option
    - One can now pass convert_values_to_numeric: { except: [key1,key2] }
    - TODO add warning when key in except does not exist in header

--- lib/smarter_csv/smarter_csv.rb
+++ lib/smarter_csv/smarter_csv.rb
@@ -103,6 +103,9 @@ module SmarterCSV
         hash.delete_if{|k,v| v =~ options[:remove_values_matching]} if options[:remove_values_matching]
         if options[:convert_values_to_numeric]
           hash.each do |k,v|
+            if options[:convert_values_to_numeric].is_a?(Hash) && options[:convert_values_to_numeric].has_key?(:except)
+              next if Array(options[:convert_values_to_numeric][:except]).include?(k)
+            end
             case v
             when /^[+-]?\d+\.\d+$/
               hash[k] = v.to_f
