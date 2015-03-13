454a47daeab4de47dc9c593e9b403944c85f96ad
Carlos Agarie <carlos.agarie@gmail.com>
2015-03-13 19:07:14 -0300

    Fix spacing in example

--- lib/extensions/hash.rb
+++ lib/extensions/hash.rb
@@ -2,7 +2,7 @@ class Hash
 
   # Example:
   #     Hash.zip(["a", "b", "c"], [1, 2, 3])
-  #     # => {"a" => 1, "b" => 2, "c" => 3}
+  #     # => { "a" => 1, "b" => 2, "c" => 3 }
   def self.zip(keys, values)
     self[keys.zip(values)]
   end
