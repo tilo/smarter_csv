ed7e611b70e1985e96849645650a8ae3865c6073
shishir127 <shishir127@gmail.com>
2015-11-19 12:35:22 +0530

    Got rid of all rspec depreciation warnings

--- spec/smarter_csv/carriage_return_spec.rb
+++ spec/smarter_csv/carriage_return_spec.rb
@@ -91,80 +91,80 @@ describe 'process files with line endings in automatic mode' do
   it 'should process a file with \n for line endings and within data fields' do
     sep = "\n"
     data = SmarterCSV.process("#{fixture_path}/carriage_returns_n.csv", {:row_sep => :auto})
-    data.flatten.size.should == 8
-    data[0][:name].should == "Anfield"
-    data[0][:street].should == "Anfield Road"
-    data[0][:city].should == "Liverpool"
-    data[1][:name].should == ["Highbury", "Highbury House"].join(sep)
-    data[2][:street].should == ["Sir Matt ", "Busby Way"].join(sep)
-    data[3][:city].should == ["Newcastle-upon-tyne ", "Tyne and Wear"].join(sep)
-    data[4][:name].should == ["White Hart Lane", "(The Lane)"].join(sep)
-    data[4][:street].should == ["Bill Nicholson Way ", "748 High Rd"].join(sep)
-    data[4][:city].should == ["Tottenham", "London"].join(sep)
-    data[5][:name].should == "Stamford Bridge"
-    data[5][:street].should == ["Fulham Road", "London"].join(sep)
-    data[5][:city].should be_nil
-    data[6][:name].should == ["Etihad Stadium", "Rowsley St", "Manchester"].join(sep)
-    data[7][:name].should == "Goodison"
-    data[7][:street].should == "Goodison Road"
-    data[7][:city].should == "Liverpool"
+    expect(data.flatten.size).to eq 8
+    expect(data[0][:name]).to eq "Anfield"
+    expect(data[0][:street]).to eq "Anfield Road"
+    expect(data[0][:city]).to eq "Liverpool"
+    expect(data[1][:name]).to eq ["Highbury", "Highbury House"].join(sep)
+    expect(data[2][:street]).to eq ["Sir Matt ", "Busby Way"].join(sep)
+    expect(data[3][:city]).to eq ["Newcastle-upon-tyne ", "Tyne and Wear"].join(sep)
+    expect(data[4][:name]).to eq ["White Hart Lane", "(The Lane)"].join(sep)
+    expect(data[4][:street]).to eq ["Bill Nicholson Way ", "748 High Rd"].join(sep)
+    expect(data[4][:city]).to eq ["Tottenham", "London"].join(sep)
+    expect(data[5][:name]).to eq "Stamford Bridge"
+    expect(data[5][:street]).to eq ["Fulham Road", "London"].join(sep)
+    expect(data[5][:city]).to be_nil
+    expect(data[6][:name]).to eq ["Etihad Stadium", "Rowsley St", "Manchester"].join(sep)
+    expect(data[7][:name]).to eq "Goodison"
+    expect(data[7][:street]).to eq "Goodison Road"
+    expect(data[7][:city]).to eq "Liverpool"
   end
 
   it 'should process a file with \r for line endings and within data fields' do
     sep = "\r"
     data = SmarterCSV.process("#{fixture_path}/carriage_returns_r.csv", {:row_sep => :auto})
-    data.flatten.size.should == 8
-    data[0][:name].should == "Anfield"
-    data[0][:street].should == "Anfield Road"
-    data[0][:city].should == "Liverpool"
-    data[1][:name].should == ["Highbury", "Highbury House"].join(sep)
-    data[2][:street].should == ["Sir Matt ", "Busby Way"].join(sep)
-    data[3][:city].should == ["Newcastle-upon-tyne ", "Tyne and Wear"].join(sep)
-    data[4][:name].should == ["White Hart Lane", "(The Lane)"].join(sep)
-    data[4][:street].should == ["Bill Nicholson Way ", "748 High Rd"].join(sep)
-    data[4][:city].should == ["Tottenham", "London"].join(sep)
-    data[5][:name].should == "Stamford Bridge"
-    data[5][:street].should == ["Fulham Road", "London"].join(sep)
-    data[5][:city].should be_nil
-    data[6][:name].should == ["Etihad Stadium", "Rowsley St", "Manchester"].join(sep)
-    data[7][:name].should == "Goodison"
-    data[7][:street].should == "Goodison Road"
-    data[7][:city].should == "Liverpool"
+    expect(data.flatten.size).to eq 8
+    expect(data[0][:name]).to eq "Anfield"
+    expect(data[0][:street]).to eq "Anfield Road"
+    expect(data[0][:city]).to eq "Liverpool"
+    expect(data[1][:name]).to eq ["Highbury", "Highbury House"].join(sep)
+    expect(data[2][:street]).to eq ["Sir Matt ", "Busby Way"].join(sep)
+    expect(data[3][:city]).to eq ["Newcastle-upon-tyne ", "Tyne and Wear"].join(sep)
+    expect(data[4][:name]).to eq ["White Hart Lane", "(The Lane)"].join(sep)
+    expect(data[4][:street]).to eq ["Bill Nicholson Way ", "748 High Rd"].join(sep)
+    expect(data[4][:city]).to eq ["Tottenham", "London"].join(sep)
+    expect(data[5][:name]).to eq "Stamford Bridge"
+    expect(data[5][:street]).to eq ["Fulham Road", "London"].join(sep)
+    expect(data[5][:city]).to be_nil
+    expect(data[6][:name]).to eq ["Etihad Stadium", "Rowsley St", "Manchester"].join(sep)
+    expect(data[7][:name]).to eq "Goodison"
+    expect(data[7][:street]).to eq "Goodison Road"
+    expect(data[7][:city]).to eq "Liverpool"
   end
 
   it 'should process a file with \r\n for line endings and within data fields' do
     sep = "\r\n"
     data = SmarterCSV.process("#{fixture_path}/carriage_returns_rn.csv", {:row_sep => :auto})
-    data.flatten.size.should == 8
-    data[0][:name].should == "Anfield"
-    data[0][:street].should == "Anfield Road"
-    data[0][:city].should == "Liverpool"
-    data[1][:name].should == ["Highbury", "Highbury House"].join(sep)
-    data[2][:street].should == ["Sir Matt ", "Busby Way"].join(sep)
-    data[3][:city].should == ["Newcastle-upon-tyne ", "Tyne and Wear"].join(sep)
-    data[4][:name].should == ["White Hart Lane", "(The Lane)"].join(sep)
-    data[4][:street].should == ["Bill Nicholson Way ", "748 High Rd"].join(sep)
-    data[4][:city].should == ["Tottenham", "London"].join(sep)
-    data[5][:name].should == "Stamford Bridge"
-    data[5][:street].should == ["Fulham Road", "London"].join(sep)
-    data[5][:city].should be_nil
-    data[6][:name].should == ["Etihad Stadium", "Rowsley St", "Manchester"].join(sep)
-    data[7][:name].should == "Goodison"
-    data[7][:street].should == "Goodison Road"
-    data[7][:city].should == "Liverpool"
+    expect(data.flatten.size).to eq 8
+    expect(data[0][:name]).to eq "Anfield"
+    expect(data[0][:street]).to eq "Anfield Road"
+    expect(data[0][:city]).to eq "Liverpool"
+    expect(data[1][:name]).to eq ["Highbury", "Highbury House"].join(sep)
+    expect(data[2][:street]).to eq ["Sir Matt ", "Busby Way"].join(sep)
+    expect(data[3][:city]).to eq ["Newcastle-upon-tyne ", "Tyne and Wear"].join(sep)
+    expect(data[4][:name]).to eq ["White Hart Lane", "(The Lane)"].join(sep)
+    expect(data[4][:street]).to eq ["Bill Nicholson Way ", "748 High Rd"].join(sep)
+    expect(data[4][:city]).to eq ["Tottenham", "London"].join(sep)
+    expect(data[5][:name]).to eq "Stamford Bridge"
+    expect(data[5][:street]).to eq ["Fulham Road", "London"].join(sep)
+    expect(data[5][:city]).to be_nil
+    expect(data[6][:name]).to eq ["Etihad Stadium", "Rowsley St", "Manchester"].join(sep)
+    expect(data[7][:name]).to eq "Goodison"
+    expect(data[7][:street]).to eq "Goodison Road"
+    expect(data[7][:city]).to eq "Liverpool"
   end
 
   it 'should process a file with more quoted text carriage return characters (\r) than line ending characters (\n)' do
     row_sep = "\n"
     text_sep = "\r"
     data = SmarterCSV.process("#{fixture_path}/carriage_returns_quoted.csv", {:row_sep => :auto})
-    data.flatten.size.should == 2
-    data[0][:band].should == "New Order"
-    data[0][:members].should == ["Bernard Sumner", "Peter Hook", "Stephen Morris", "Gillian Gilbert"].join(text_sep)
-    data[0][:albums].should == ["Movement", "Power, Corruption and Lies", "Low-Life", "Brotherhood", "Substance"].join(text_sep)
-    data[1][:band].should == "Led Zeppelin"
-    data[1][:members].should == ["Jimmy Page", "Robert Plant", "John Bonham", "John Paul Jones"].join(text_sep)
-    data[1][:albums].should == ["Led Zeppelin", "Led Zeppelin II", "Led Zeppelin III", "Led Zeppelin IV"].join(text_sep)
+    expect(data.flatten.size).to eq 2
+    expect(data[0][:band]).to eq "New Order"
+    expect(data[0][:members]).to eq ["Bernard Sumner", "Peter Hook", "Stephen Morris", "Gillian Gilbert"].join(text_sep)
+    expect(data[0][:albums]).to eq ["Movement", "Power, Corruption and Lies", "Low-Life", "Brotherhood", "Substance"].join(text_sep)
+    expect(data[1][:band]).to eq "Led Zeppelin"
+    expect(data[1][:members]).to eq ["Jimmy Page", "Robert Plant", "John Bonham", "John Paul Jones"].join(text_sep)
+    expect(data[1][:albums]).to eq ["Led Zeppelin", "Led Zeppelin II", "Led Zeppelin III", "Led Zeppelin IV"].join(text_sep)
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ require 'smarter_csv'
 
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
 #  config.fixture_path = 'spec/fixtures'
