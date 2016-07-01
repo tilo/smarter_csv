dcb291462e9d70f702f173b0c721de0320cb98bd
akm <akm2000@gmail.com>
2016-07-01 19:19:32 +0900

    Fix the regular expression to remove quote char

--- lib/smarter_csv/smarter_csv.rb
+++ lib/smarter_csv/smarter_csv.rb
@@ -107,7 +107,7 @@ module SmarterCSV
         else
           dataA =  line.split(options[:col_sep])
         end
-        dataA.map!{|x| x.gsub(%r/options[:quote_char]/,'') }
+        dataA.map!{|x| x.gsub(%r/#{options[:quote_char]}/,'') }
         dataA.map!{|x| x.strip}  if options[:strip_whitespace]
         hash = Hash.zip(headerA,dataA)  # from Facets of Ruby library
         # make sure we delete any key/value pairs from the hash, which the user wanted to delete:
