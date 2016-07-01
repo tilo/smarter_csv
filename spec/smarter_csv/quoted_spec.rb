397a50687de01b8b70d12afd586660a00d642af9
akm <akm2000@gmail.com>
2016-07-01 19:19:09 +0900

    Add an example to reproduce the problem

--- spec/smarter_csv/quoted_spec.rb
+++ spec/smarter_csv/quoted_spec.rb
@@ -3,11 +3,25 @@ require 'spec_helper'
 fixture_path = 'spec/fixtures'
 
 describe 'be_able_to' do
-  it 'loads_file_with_quoted_fields' do 
+  let(:quoted_model_patten){ /\A[^\"]+\"[^\"]+\"\z/ }
+
+  it 'loads_file_with_quoted_fields' do
     options = {}
     data = SmarterCSV.process("#{fixture_path}/quoted.csv", options)
     data.flatten.size.should == 4
+    data[1][:model].should match quoted_model_patten
     data[1][:description].should be_nil
+    data[2][:model].should match quoted_model_patten
     data[2][:description].should be_nil
+  end
+
+  it 'loads_file_with_quoted_fields' do
+    options = {:force_simple_split => true}
+    data = SmarterCSV.process("#{fixture_path}/quoted.csv", options)
+    data.flatten.size.should == 4
+    data[1][:model].should match quoted_model_patten
+    data[1][:description].should be_nil
+    data[2][:model].should match /\A[^\"]+\"[^\"]+\z/
+    data[2][:description].should match /\A[^\"]+\"\z/
   end
 end
