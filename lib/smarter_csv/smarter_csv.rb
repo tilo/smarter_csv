f992a6727d9948cf2e897d811a31dd89f2275fc1
Waldyr de Souza <waldyr.ar@gmail.com>
2016-11-26 18:34:59 -0300

    Close file after job done (#80)
    * Close file after processing it
    
    * Remove obnoxious comment

--- lib/smarter_csv/smarter_csv.rb
+++ lib/smarter_csv/smarter_csv.rb
@@ -44,8 +44,6 @@ module SmarterCSV
           file_headerA.map!{|x| x.downcase }   if options[:downcase_header]
         end
 
-#        puts "HeaderA: #{file_headerA.join(' , ')}" if options[:verbose]
-
         file_header_size = file_headerA.size
       else
         raise SmarterCSV::IncorrectOption , "ERROR [smarter_csv]: If :headers_in_file is set to false, you have to provide :user_provided_headers" if options[:user_provided_headers].nil?
@@ -189,6 +187,7 @@ module SmarterCSV
       end
     ensure
       $/ = old_row_sep   # make sure this stupid global variable is always reset to it's previous value after we're done!
+      f.close
     end
     if block_given?
       return chunk_count  # when we do processing through a block we only care how many chunks we processed
