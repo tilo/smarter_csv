2c0bb4406b050c8ab18ea25f79433670094b11d8
Tilo Sloboda <tilo.sloboda@gmail.com>
2018-01-28 21:21:33 -0800

    adding hash_validations for required_fields_matching

--- lib/smarter_csv/hash_validations.rb
+++ lib/smarter_csv/hash_validations.rb
@@ -3,7 +3,9 @@ module SmarterCSV
   # all these take the hash as the input
   #
   # the computed options can be accessed via @options
-
+  #
+  # required_fields: [:first_name, :last_name, :age] # e.g. can't be blank or zero
+  #
   def self.required_fields(hash, required=[])
     @@required_fields ||= Proc.new {|hash, required=[]|
       raise( SmarterCSV::IncorrectOption , "ERROR: required_fields validation needs an array argument" ) unless required.is_a?(Array)
@@ -18,5 +20,25 @@ module SmarterCSV
       count
     }
     @@required_fields.call(hash,required)
+  end
+
+  #
+  # required_fields_matching: {
+  #   first_name: /\A\w+\z/, last_name: /\A\w+\z/, employee_number: /\A[0-9]\d+\z/, email: /\A[^@]+@[^@]+\z/
+  # }
+  def self.required_fields_matching(hash, requirements_hash={})
+    @@required_fields_matching ||= Proc.new {|hash, requirements_hash={}|
+      raise( SmarterCSV::IncorrectOption , "ERROR: required_fields_matching validation needs a hash argument" ) unless requirements_hash.is_a?(Hash)
+      count = 0
+      requirements_hash.each do |key,regex|
+        if ! hash.keys.include?(key) || hash[key] !~ regex
+          @errors[ @file_line_count ] ||= []
+          @errors[ @file_line_count ] << "Field `#{key.inspect}` in CSV line #{@file_line_count}: `#{hash[key]}` did not match #{regex.inspect}"
+          count += 1
+        end
+      end
+      count
+    }
+    @@required_fields_matching.call(hash,requirements_hash)
   end
 end
--- spec/smarter_csv/hash_validations_spec.rb
+++ spec/smarter_csv/hash_validations_spec.rb
@@ -31,4 +31,26 @@ describe 'hash validations' do
     SmarterCSV.warnings.should eq({4=>["No data in line 4"], 8=>["No data in line 8"]})
   end
 
+  it 'does required_fields_matching' do
+    # this requires emails matching domain bla.com, with one word in front
+    # this requires employee_id to start with a sequence of zeros
+    # this requires first_name and last_name to be words
+    options = {
+      hash_validations: [
+        required_fields_matching: {
+          first_name: /\A[[:word:]]+\z/, last_name: /\A[[:word:]]+\z/, employee_id: /\A0+\d+\z/,
+          email: /\A\w+@bla.com\z/, manager_email: /\A\w+@bla.com\z/
+        }
+      ]
+    }
+    data = SmarterCSV.process("#{fixture_path}/user_import_bad.csv", options)
+
+    data.size.should eq 2
+    SmarterCSV.errors.keys.size.should eq 2
+    SmarterCSV.errors.should eq({
+      2=>["Field `:manager_email` in CSV line 2: `tom@blubb.com` did not match /\\A\\w+@bla.com\\z/"],
+      5=>["Field `:employee_id` in CSV line 5: `17` did not match /\\A0+\\d+\\z/", "Field `:email` in CSV line 5: `danny.0000@bla.com` did not match /\\A\\w+@bla.com\\z/"]
+    })
+    SmarterCSV.warnings.should eq Hash.new
+  end
 end
