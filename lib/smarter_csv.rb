a40da934491b0e3b0f671cd6488c149666c5100c
Tilo Sloboda <tilo.sloboda@gmail.com>
2022-02-13 21:00:05 -0800

    fix

--- lib/smarter_csv.rb
+++ lib/smarter_csv.rb
@@ -1,7 +1,9 @@
-require 'simplecov'
-SimpleCov.start do
-  add_filter "/spec/"
-  add_filter "/pkg/"
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter "/spec/"
+    add_filter "/pkg/"
+  end
 end
 
 require 'csv'
