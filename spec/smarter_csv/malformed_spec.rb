b11155def250c1a8ae560a6f0ccf2eccdebb366f
Tilo Sloboda <tilo.sloboda@gmail.com>
2022-05-02 08:53:35 +0800

    test

--- spec/smarter_csv/malformed_spec.rb
+++ spec/smarter_csv/malformed_spec.rb
@@ -3,15 +3,19 @@ require 'spec_helper'
 fixture_path = 'spec/fixtures'
 
 describe 'malformed_csv' do
-  subject { lambda { SmarterCSV.process(csv_path) } }
-
   context "malformed header" do
     let(:csv_path) { "#{fixture_path}/malformed_header.csv" }
-    it { should raise_error(CSV::MalformedCSVError) }
+
+    it 'raises exception' do
+      expect{ SmarterCSV.process(csv_path) }.to raise_error(CSV::MalformedCSVError, anything)
+    end
   end
 
   context "malformed content" do
     let(:csv_path) { "#{fixture_path}/malformed.csv" }
-    it { should raise_error(CSV::MalformedCSVError) }
+
+    it 'raises exception' do
+      expect{ SmarterCSV.process(csv_path) }.to raise_error(CSV::MalformedCSVError, anything)
+    end
   end
 end
