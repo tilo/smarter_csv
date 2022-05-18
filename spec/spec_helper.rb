95ac8e1d58a29a32a894c8ec5ee44df21c211bf0
Tilo Sloboda <tsloboda@chime.com>
2022-05-18 06:21:18 -0700

    update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,17 +5,15 @@ Bundler.require(:default)
 
 require 'smarter_csv'
 
-
-RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.filter_run :focus => true
-  config.run_all_when_everything_filtered = true
-#  config.fixture_path = 'spec/fixtures'
-
-#  config.mock_with :rr
-#  config.before(:each) do
-#    Project.delete_all
-#    Category.delete_all
-#  end
+SimpleCov.start do
+  add_filter /spec/
 end
 
+RSpec.configure do |config|
+
+  config.filter_run_when_matching :focus
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  config.run_all_when_everything_filtered = true
+end
