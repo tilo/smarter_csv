88a8d88fd647e28d8dbedead04eda13099d3d47e
Tilo Sloboda <tilo.sloboda@gmail.com>
2022-06-05 15:21:55 -0700

    meh

--- lib/smarter_csv.rb
+++ lib/smarter_csv.rb
@@ -1,5 +1,5 @@
 require 'csv'
 require "smarter_csv/version"
 require "extensions/hash.rb"
-require "./ext/smarter_csv/parse_csv_line"
+#require "./ext/smarter_csv/parse_csv_line"
 require "smarter_csv/smarter_csv.rb"
