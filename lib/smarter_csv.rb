aae66ca74d813e1268348068e602ac282110e6a1
Tilo Sloboda <tsloboda@chime.com>
2022-07-31 20:25:09 -0700

    update

--- lib/smarter_csv.rb
+++ lib/smarter_csv.rb
@@ -247,12 +247,12 @@ module SmarterCSV
       # puts "SmarterCSV.parse OPTIONS: #{options[:acceleration]}" if options[:verbose]
 
       if options[:acceleration] && has_acceleration?
-        # puts "NOTICE: Accelerated SmarterCSV / #{options[:acceleration]}" if options[:verbose]
+        # :nocov:
         has_quotes = line =~ /#{options[:quote_char]}/
         elements = parse_csv_line_c(line, options[:col_sep], options[:quote_char], header_size)
         elements.map!{|x| cleanup_quotes(x, options[:quote_char])} if has_quotes
         return [elements, elements.size]
-
+        # :nocov:
       else
         # puts "WARNING: SmarterCSV is using un-accelerated parsing of lines. Check options[:acceleration]"
         return parse_csv_line_ruby(line, options, header_size)
