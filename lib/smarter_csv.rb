0615e7de0d97225a40e6bb3e1ea22eaba0178b41
Tilo Sloboda <tilo.sloboda@gmail.com>
2022-08-28 23:14:19 -0700

    version 1.7.2

--- lib/smarter_csv.rb
+++ lib/smarter_csv.rb
@@ -2,9 +2,9 @@
 
 require_relative "extensions/hash"
 require_relative "smarter_csv/version"
-#require_relative "smarter_csv/smarter_csv" unless ENV['CI'] # does not compile/link in CI?
 
-require 'smarter_csv.bundle' unless ENV['CI'] # does not compile/link in CI?
+require_relative "smarter_csv/smarter_csv" unless ENV['CI'] # does not compile/link in CI?
+# require 'smarter_csv.bundle' unless ENV['CI'] # does not compile/link in CI?
 
 module SmarterCSV
   class SmarterCSVException < StandardError; end
