53e62e5c1c9bd00a7a3621a406be6cca283c792e
Tilo Sloboda <tilo.sloboda@gmail.com>
2023-02-13 07:52:21 -0800

    update

--- spec/smarter_csv/validations_spec.rb
+++ spec/smarter_csv/validations_spec.rb
@@ -5,6 +5,8 @@ require 'spec_helper'
 fixture_path = 'spec/fixtures'
 
 describe 'validations' do
+  let(:options) { {} }
+
   it 'loads basic csv file without issues' do
     data = SmarterCSV.process("#{fixture_path}/basic.csv", options)
     expect(data.size).to eq 5
@@ -13,7 +15,7 @@ describe 'validations' do
   [:row_sep, :col_sep, :quote_char].each do |option|
     [nil, :symbol, 1].each do |value|
     context "with #{option} set to #{value}" do
-      let(:options) { option => value}
+      let(:options) { { option => value } }
 
       it "raises an exception if #{option} is #{value}" do
         expect { SmarterCSV.process("#{fixture_path}/basic.csv", options) }.to raise_exception(SmarterCSV::ValidationError)
@@ -22,7 +24,7 @@ describe 'validations' do
   end
 
   context "does not raise an exception if #{option} is a string" do
-    let(:options) { option => 'a'}
+    let(:options) { { option => 'a'} }
     it 'loads basic csv file without issues' do
       data = SmarterCSV.process("#{fixture_path}/basic.csv", options)
       expect(data.size).to be >= 1
