965b8c868c15a47e94fe27ac3a6e96736808be67
Tilo Sloboda <tilo.sloboda@gmail.com>
2023-03-18 14:44:26 -0700

    adding more tests

--- lib/smarter_csv.rb
+++ lib/smarter_csv.rb
@@ -243,7 +243,7 @@ module SmarterCSV
       line = filehandle.readline(options[:row_sep])
       @file_line_count += 1
       @csv_line_count += 1
-      line = remove_bom(line) if @file_line_count == 1
+      line = remove_bom(line) if @csv_line_count == 1
       line
     end
 
@@ -526,22 +526,22 @@ module SmarterCSV
 
     private
 
-    UTF_32_BOM = %w[00 00 fe ff].freeze
-    UTF_32LE_BOM = %w[ff fe 00 00].freeze
+    UTF_32_BOM = %w[0 0 fe ff].freeze
+    UTF_32LE_BOM = %w[ff fe 0 0].freeze
     UTF_8_BOM = %w[ef bb bf].freeze
-    UTF_16_BOM = %w[ef ff].freeze
-    UTF_16LE_BOM = %w[ff ef].freeze
+    UTF_16_BOM = %w[fe ff].freeze
+    UTF_16LE_BOM = %w[ff fe].freeze
 
     def remove_bom(str)
       str_as_hex = str.bytes.map{|x| x.to_s(16)}
       # if string does not start with one of the bytes above, there is no BOM
-      return str unless %w[ef ff 00].include?(str_as_hex[0])
+      return str unless %w[ef fe ff 0].include?(str_as_hex[0])
 
       return str.byteslice(4..-1) if [UTF_32_BOM, UTF_32LE_BOM].include?(str_as_hex[0..3])
       return str.byteslice(3..-1) if str_as_hex[0..2] == UTF_8_BOM
       return str.byteslice(2..-1) if [UTF_16_BOM, UTF_16LE_BOM].include?(str_as_hex[0..1])
 
-      puts "SmarterCSV found unhandled BOM! #{str.cars[0..7].inspect}"
+      puts "SmarterCSV found unhandled BOM! #{str.chars[0..7].inspect}"
       str
     end
 
--- spec/smarter_csv/bom_issues_spec.rb
+++ spec/smarter_csv/bom_issues_spec.rb
@@ -8,67 +8,49 @@ describe 'BOM Tests' do
   let(:options) { {} }
   subject(:data) { SmarterCSV.process(file, options) }
 
-  context 'when given CSV file with UTF-8 BOM EF BB BF' do
-    let(:file) { "#{fixture_path}/bom_test_efbbbf.csv" }
-
-    it 'loads all lines in the file' do
-      expect(data.size).to eq 2
-    end
-
-    it 'loads the file with the correct headers' do
-      expect(data[0].keys).to eq [:some_id, :type, :fuzzboxes]
-    end
-
-    it 'strips the BOM' do
-      expect(data[0][:some_id]).not_to be_nil # untreated BOM issue would taint first column's symbol with the BOM
-    end
-
-    it 'can access the first column values' do
-      expect(data[0][:some_id]).to eq 42_766_805
-      expect(data[1][:some_id]).to eq 38_759_150
-    end
-
-    context 'when no header in file' do
-      let(:options) { {headers_in_file: false, user_provided_headers: %i[one two three]} }
+  %w[fffe feff efbbbf fffe0000 0000feff].each do |bom|
+    context "when given CSV file with UTF-8 BOM #{bom}" do
+      let(:file) { "#{fixture_path}/bom_test_#{bom}.csv" }
 
       it 'loads all lines in the file' do
-        expect(data.size).to eq 3 # it interprets the first line as data
+        expect(data.size).to eq 2
       end
 
       it 'loads the file with the correct headers' do
-        expect(data[0].keys).to eq [:one, :two, :three]
+        expect(data[0].keys).to eq [:some_id, :type, :fuzzboxes]
       end
 
       it 'strips the BOM' do
-        expect(data[0][:one]).not_to be_nil # untreated BOM issue would taint first column's symbol with the BOM
+        expect(data[0][:some_id]).not_to be_nil # untreated BOM issue would taint first column's symbol with the BOM
       end
 
       it 'can access the first column values' do
-        expect(data[0][:one]).to eq 'some_id'
-        expect(data[1][:one]).to eq 42_766_805
-        expect(data[2][:one]).to eq 38_759_150
+        expect(data[0][:some_id]).to eq 42_766_805
+        expect(data[1][:some_id]).to eq 38_759_150
       end
-    end
-  end
 
-  context 'when given CSV file with UTF-16 BOM EF FF' do
-    let(:file) { "#{fixture_path}/bom_test_efff.csv" }
+      # we fake that there is no header in the file
+      context 'when no header in file' do
+        let(:options) { {headers_in_file: false, user_provided_headers: %i[one two three]} }
 
-    it 'loads all lines in the file' do
-      expect(data.size).to eq 9
-    end
+        it 'loads all lines in the file' do
+          expect(data.size).to eq 3 # it interprets the first line as data
+        end
 
-    it 'loads the file with the correct headers' do
-      expect(data[0].keys).to eq [:user_id]
-    end
+        it 'loads the file with the correct headers' do
+          expect(data[0].keys).to eq [:one, :two, :three]
+        end
 
-    it 'strips the BOM' do
-      expect(data[0][:user_id]).not_to be_nil # untreated BOM issue would taint first column's symbol with the BOM
-    end
+        it 'strips the BOM' do
+          expect(data[0][:one]).not_to be_nil # untreated BOM issue would taint first column's symbol with the BOM
+        end
 
-    it 'can access the first column values' do
-      expect(data.first[:user_id]).to eq 34_194_955
-      expect(data.last[:user_id]).to eq 3_019_053
+        it 'can access the first column values' do
+          expect(data[0][:one]).to eq 'some_id'
+          expect(data[1][:one]).to eq 42_766_805
+          expect(data[2][:one]).to eq 38_759_150
+        end
+      end
     end
   end
 end
