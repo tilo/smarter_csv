7ec60ca3dd0a4acf7adbdb8c7ee7da60cbe52695
Tilo Sloboda <tilo.sloboda@gmail.com>
2023-03-19 17:13:40 -0700

    test cleanup

--- spec/smarter_csv/quoted_spec.rb
+++ spec/smarter_csv/quoted_spec.rb
@@ -5,36 +5,42 @@ require 'spec_helper'
 fixture_path = 'spec/fixtures'
 
 describe 'loading file with quoted fields' do
-  it 'leaving the quotes in the data' do
-    options = {}
-    data = SmarterCSV.process("#{fixture_path}/quoted.csv", options)
-    expect(data.flatten.size).to eq 4
-    expect(data[1][:model]).to eq 'Venture "Extended Edition"'
-    expect(data[1][:description]).to be_nil
-    expect(data[2][:model]).to eq 'Venture "Extended Edition, Very Large"'
-    expect(data[2][:description]).to be_nil
-    expect(data[3][:description]).to eq 'MUST SELL! air, moon roof, loaded'
-    data.each do |h|
-      expect(h[:year].class).to eq Fixnum
-      expect(h[:make]).to_not be_nil
-      expect(h[:model]).to_not be_nil
-      expect(h[:price].class).to eq Float
+  let(:options) { {} }
+  subject(:data) { SmarterCSV.process(file, options) }
+
+  describe 'file quoted.csv' do
+    let(:file) { "#{fixture_path}/quoted.csv" }
+
+    it 'leaving the quotes in the data' do
+      expect(data.flatten.size).to eq 4
+      expect(data[1][:model]).to eq 'Venture "Extended Edition"'
+      expect(data[1][:description]).to be_nil
+      expect(data[2][:model]).to eq 'Venture "Extended Edition, Very Large"'
+      expect(data[2][:description]).to be_nil
+      expect(data[3][:description]).to eq 'MUST SELL! air, moon roof, loaded'
+      data.each do |h|
+        expect(h[:year].class).to eq Fixnum
+        expect(h[:make]).to_not be_nil
+        expect(h[:model]).to_not be_nil
+        expect(h[:price].class).to eq Float
+      end
     end
   end
 
   # quotes inside quoted fields need to be escaped by another double-quote
-  it 'removes quotes around quoted fields, but not inside data' do
-    options = {}
-    data = SmarterCSV.process("#{fixture_path}/quote_char.csv", options)
+  describe 'file quote_char.csv' do
+    let(:file) { "#{fixture_path}/quote_char.csv" }
 
-    expect(data.length).to eq 6
-    expect(data[0][:first_name]).to eq "\"John"
-    expect(data[0][:last_name]).to eq "Cooke\""
-    expect(data[1][:first_name]).to eq "Jam\ne\nson\""
-    expect(data[2][:first_name]).to eq "\"Jean"
-    expect(data[4][:first_name]).to eq "Bo\"bbie"
-    expect(data[5][:first_name]).to eq 'Mica'
-    expect(data[5][:last_name]).to eq 'Copeland'
+    it 'removes quotes around quoted fields, but not inside data' do
+      expect(data.length).to eq 6
+      expect(data[0][:first_name]).to eq "\"John"
+      expect(data[0][:last_name]).to eq "Cooke\""
+      expect(data[1][:first_name]).to eq "Jam\ne\nson\""
+      expect(data[2][:first_name]).to eq "\"Jean"
+      expect(data[4][:first_name]).to eq "Bo\"bbie"
+      expect(data[5][:first_name]).to eq 'Mica'
+      expect(data[5][:last_name]).to eq 'Copeland'
+    end
   end
 
   # NOTE: quotes inside headers need to be escaped by doubling them
@@ -42,13 +48,14 @@ describe 'loading file with quoted fields' do
   #       this escaping is illegal: 'incorrect \"EXAMPLE\"' <-- this caused CSV parsing error
   #  in case of CSV parsing errirs, use :user_provided_headers, or key_mapping
   #
-  it 'removes quotes around headers and extra quotes inside headers' do
-    options = {}
-    data = SmarterCSV.process("#{fixture_path}/quoted2.csv", options)
+  describe 'file quoted2.csv' do
+    let(:file) { "#{fixture_path}/quoted2.csv" }
 
-    expect(data.length).to eq 3
-    expect(data.first.keys[2]).to eq :isbn
-    expect(data.first.keys[3]).to eq :discounted_price
-    expect(data[1][:author]).to eq 'Timothy "The Parser" Campbell'
+    it 'removes quotes around headers and extra quotes inside headers' do
+      expect(data.length).to eq 3
+      expect(data.first.keys[2]).to eq :isbn
+      expect(data.first.keys[3]).to eq :discounted_price
+      expect(data[1][:author]).to eq 'Timothy "The Parser" Campbell'
+    end
   end
 end
