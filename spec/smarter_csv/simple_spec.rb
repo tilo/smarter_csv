bdef2687edd289335fd420a1a67fc5ff3bd8c2ab
Tilo Sloboda <tsloboda@chime.com>
2023-03-21 11:12:37 -0700

    simplify!

--- spec/smarter_csv/simple_spec.rb
+++ spec/smarter_csv/simple_spec.rb
@@ -4,11 +4,24 @@ require 'spec_helper'
 
 fixture_path = 'spec/fixtures'
 
-describe 'simple file' do
-  let(:options) { {} }
-  subject(:data) { SmarterCSV.process("#{fixture_path}/simple.csv", options) }
 
-  it 'loads the csv file without issues' do
-    expect(data.size).to eq 4
+describe 'handling files with one column' do
+  let(:options) { {} }
+  subject(:data) { SmarterCSV.process(file, options) }
+
+  context 'when simple file with header' do
+    let(:file) { "#{fixture_path}/simple_with_header.csv" }
+
+    xit 'loads the csv file without issues' do
+      expect(data.size).to eq 4 # should not raise
+    end
+  end
+
+  context 'when simple file without header' do
+    let(:file) { "#{fixture_path}/simple_no_header.csv" }
+
+    xit 'loads the csv file without issues' do
+      expect(data.size).to eq 4 # should not raise
+    end
   end
 end
