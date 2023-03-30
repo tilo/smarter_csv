dd29cfd2cb2e8365016fe9450603480df12e0c30
Tilo Sloboda <tsloboda@chime.com>
2023-03-30 12:37:58 -0700

    Fix issue #229 / windows one-column files

--- spec/smarter_csv/simple_spec.rb
+++ spec/smarter_csv/simple_spec.rb
@@ -9,7 +9,7 @@ describe 'handling files with one column' do
   let(:options) { {} }
   subject(:data) { SmarterCSV.process(file, options) }
 
-  context 'when simple file with header' do
+  context 'when simple unix file with header' do
     let(:file) { "#{fixture_path}/simple_with_header.csv" }
 
     it 'loads the csv file without issues' do
@@ -17,7 +17,15 @@ describe 'handling files with one column' do
     end
   end
 
-  context 'when simple file without header' do
+  context 'when simple windows file with header' do
+    let(:file) { "#{fixture_path}/simple_with_header_windows.csv" }
+
+    it 'loads the csv file without issues' do
+      expect(data.size).to eq 4 # should not raise
+    end
+  end
+
+  context 'when simple unix file without header' do
     let(:file) { "#{fixture_path}/simple_no_header.csv" }
 
     it 'loads the csv file without issues' do
@@ -27,8 +35,26 @@ describe 'handling files with one column' do
     end
   end
 
-  context 'when simple file with header and UTF-8 chars' do
+  context 'when simple windows file without header' do
+    let(:file) { "#{fixture_path}/simple_no_header_windows.csv" }
+
+    it 'loads the csv file without issues' do
+      options[:headers_in_file] = false
+      options[:user_provided_headers] = ['this']
+      expect(data.size).to eq 4 # should not raise
+    end
+  end
+
+  context 'when simple unix file with header and UTF-8 chars' do
     let(:file) { "#{fixture_path}/simple_with_header_utf8.csv" }
+
+    it 'loads the csv file without issues' do
+      expect{ data }.to raise_exception(SmarterCSV::NoColSepDetected)
+    end
+  end
+
+  context 'when simple windows file with header and UTF-8 chars' do
+    let(:file) { "#{fixture_path}/simple_with_header_utf8_windows.csv" }
 
     it 'loads the csv file without issues' do
       expect{ data }.to raise_exception(SmarterCSV::NoColSepDetected)
