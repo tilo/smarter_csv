ba8aff3112583c5cb844efadcfdf270db486cf22
Tilo Sloboda <tilo.sloboda@gmail.com>
2023-10-30 11:51:04 -0700

    testing loading of library

--- lib/smarter_csv.rb
+++ lib/smarter_csv.rb
@@ -4,47 +4,52 @@ require "core_ext/hash"
 
 require "smarter_csv/version"
 require "smarter_csv/options_processing"
+
 # require "smarter_csv/smarter_csv"
 
 
-# require_relative "smarter_csv/smarter_csv" unless ENV['CI'] # does not compile/link in CI?
-require 'smarter_csv.bundle' unless ENV['CI'] # local testing
+
+require 'smarter_csv.bundle' if `uname -s`.chomp == 'Darwin'
+
+require_relative 'smarter_csv/smarter_csv'
+
+
 # require_relative "smarter_csv/smarter_csv" unless ENV['CI'] # does not compile/link in CI?
 # require 'smarter_csv.bundle' unless ENV['CI'] # local testing
 
-if RUBY_ENGINE == 'ruby'
-  path = `find tmp -name smarter_csv`.chomp
-  if path.empty?
-    # :nocov:
-    puts "\n\nCOULD NOT DETERMINE PATH\n\n"
-    require_relative "smarter_csv/smarter_csv"
-    # :nocov:
-  else
+# if RUBY_ENGINE == 'ruby'
+#   path = `find tmp -name smarter_csv`.chomp
+#   if path.empty?
+#     # :nocov:
+#     puts "\n\nCOULD NOT DETERMINE PATH\n\n"
+#     require_relative "smarter_csv/smarter_csv"
+#     # :nocov:
+#   else
 
-    object_path = "#{path}/#{RUBY_VERSION}/smarter_csv"
-    require_relative "../#{object_path}"
+#     object_path = "#{path}/#{RUBY_VERSION}/smarter_csv"
+#     require_relative "../#{object_path}"
 
-  end
+#   end
 
-  require 'smarter_csv/smarter_csv'
-# :nocov:
-elsif RUBY_ENGINE == 'truffleruby'
-  puts "\n\n truffleruby case in the load path | RUBY_ENGINE: #{RUBY_ENGINE} , #{RUBY_VERSION}\n\n"
-  # this might not work - if you encounter problems, please contribute and create a PR
-  # require 'truffleruby/smarter_csv'
-  require 'smarter_csv/smarter_csv'
+#   require 'smarter_csv/smarter_csv'
+# # :nocov:
+# elsif RUBY_ENGINE == 'truffleruby'
+#   puts "\n\n truffleruby case in the load path | RUBY_ENGINE: #{RUBY_ENGINE} , #{RUBY_VERSION}\n\n"
+#   # this might not work - if you encounter problems, please contribute and create a PR
+#   # require 'truffleruby/smarter_csv'
+#   require 'smarter_csv/smarter_csv'
 
-else
-  puts <<-BLOCK_COMMENT
+# else
+#   puts <<-BLOCK_COMMENT
 
-    -------------------------------------------------------------------------
-      RUBY_ENGINE: #{RUBY_ENGINE} , #{RUBY_VERSION}
+#     -------------------------------------------------------------------------
+#       RUBY_ENGINE: #{RUBY_ENGINE} , #{RUBY_VERSION}
 
-      Acceleration via C-Extension is currently not supported for #{RUBY_ENGINE}
+#       Acceleration via C-Extension is currently not supported for #{RUBY_ENGINE}
 
-      Please contribute and create a pull request if you need this
-    -------------------------------------------------------------------------
+#       Please contribute and create a pull request if you need this
+#     -------------------------------------------------------------------------
 
-  BLOCK_COMMENT
-end
-# :nocov:
+#   BLOCK_COMMENT
+# end
+# # :nocov:
--- lib/smarter_csv/version.rb
+++ lib/smarter_csv/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module SmarterCSV
-  VERSION = "1.9.1.pre2"
+  VERSION = "1.9.1.pre3"
 end
