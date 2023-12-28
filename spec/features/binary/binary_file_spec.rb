6f0f3644e4d1c2f56d362665d2f109080d8545ce
Tilo Sloboda <tilo.sloboda@gmail.com>
2023-12-28 22:18:04 +0100

    update

--- spec/features/binary/binary_file_spec.rb
+++ spec/features/binary/binary_file_spec.rb
@@ -8,10 +8,12 @@ fixture_path = 'spec/fixtures'
 # but contains control characters delimiting the rows and columns, and also
 # contains a comment section which is commented our by a leading # character
 
-describe 'be_able_to' do
+describe 'read binary files' do
+  let(:binary_file) { "#{fixture_path}/binary.csv" }
+
   it 'loads_binary_file_with_comments' do
     options = {col_sep: "\cA", row_sep: "\cB", comment_regexp: /^#/}
-    data = SmarterCSV.process("#{fixture_path}/binary.csv", options)
+    data = SmarterCSV.process(binary_file, options)
     expect(data.flatten.size).to eq 8
 
     data.each do |item|
@@ -22,6 +24,24 @@ describe 'be_able_to' do
       expect(item[:timestamp]).to eq 1_381_388_409
       expect(item[:item_id].class).to eq Integer
       expect(item[:name].size).to be > 0
+    end
+  end
+
+  # same as previous test, but reading the file with strings as keys
+
+  it 'loads_binary_file_with_strings_as_keys' do
+    options = {col_sep: "\cA", row_sep: "\cB", comment_regexp: /^#/, strings_as_keys: true}
+    data = SmarterCSV.process(binary_file, options)
+    expect(data.size).to eq 8
+
+    data.each do |item|
+      # all keys should be strings
+      item.each_key do |key|
+        expect(key.class).to eq String
+      end
+      expect(item['timestamp']).to eq 1_381_388_409
+      expect(item['item_id'].class).to eq Integer
+      expect(item['name'].size).to be > 0
     end
   end
 end
--- spec/smarter_csv/blank_spec.rb
+++ spec/smarter_csv/blank_spec.rb
@@ -77,5 +77,3 @@ describe 'blank?' do
     end
   end
 end
-
-
