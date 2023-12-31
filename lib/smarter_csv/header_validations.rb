42f260b6acb4f5cac11c907945f121dbe942c597
Tilo Sloboda <tilo.sloboda@gmail.com>
2023-12-31 08:52:26 +0100

    performance improvements

--- lib/smarter_csv/header_validations.rb
+++ lib/smarter_csv/header_validations.rb
@@ -18,6 +18,32 @@ module SmarterCSV
     end
 
     def check_duplicate_headers_v1(headers, options)
+      check_duplicate_headers_v1_new2(headers, options)
+    end
+
+    def check_duplicate_headers_v1_new2(headers, options)
+      header_counts = Hash.new(0)
+      headers.each { |header| header_counts[header] += 1 unless header.nil? }
+
+      duplicates = header_counts.select { |_, count| count > 1 }
+
+      unless duplicates.empty?
+        raise(SmarterCSV::DuplicateHeaders, "Duplicate Headers in CSV: #{duplicates.inspect}")
+      end
+    end
+
+    # def check_duplicate_headers_v1_new(headers, options)
+    #   dupes = headers.each_with_object({}) do |x, h|
+    #     # when we remove fields we map them to nil - we don't count these as dupes
+    #     h[x] ||= 0
+    #     h[x] += 1
+    #   end.reject do |k, v|
+    #     k.nil? || v < 2
+    #    end
+    #  dupes.empty? ? nil : raise(SmarterCSV::DuplicateHeaders, "ERROR: duplicate headers: #{dupes.inspect}")
+    # end
+
+    def check_duplicate_headers_v1_old(headers, options)
       duplicate_headers = []
       headers.compact.each do |k|
         duplicate_headers << k if headers.select{|x| x == k}.size > 1
@@ -29,14 +55,31 @@ module SmarterCSV
     end
 
     def check_required_headers_v1(headers, options)
+      check_required_headers_v1_new(headers, options)
+    end
+
+    require 'set'
+
+    def check_required_headers_v1_new(headers, options)
       if options[:required_keys] && options[:required_keys].is_a?(Array)
-        missing_keys = []
-        options[:required_keys].each do |k|
-          missing_keys << k unless headers.include?(k)
+        headers_set = headers.to_set
+        missing_keys = options[:required_keys].select { |k| !headers_set.include?(k) }
+
+        unless missing_keys.empty?
+          raise SmarterCSV::MissingKeys, "ERROR: missing attributes: #{missing_keys.join(',')}"
         end
-        raise SmarterCSV::MissingKeys, "ERROR: missing attributes: #{missing_keys.join(',')}" unless missing_keys.empty?
       end
     end
+
+    # def check_required_headers_v1_old(headers, options)
+    #   if options[:required_keys] && options[:required_keys].is_a?(Array)
+    #     missing_keys = []
+    #     options[:required_keys].each do |k|
+    #       missing_keys << k unless headers.include?(k)
+    #     end
+    #     raise SmarterCSV::MissingKeys, "ERROR: missing attributes: #{missing_keys.join(',')}" unless missing_keys.empty?
+    #   end
+    # end
 
     # ---- V2.x Version: validate the headers -----------------------------------------------------------------
 
@@ -68,21 +111,43 @@ module SmarterCSV
     # the computed options can be accessed via @options
 
     def unique_headers(array)
-      dupes = headers.each_with_object({}) do |x, h|
-         # when we remove fields we map them to nil - we don't count these as dupes
-         h[x] ||= 0
-         h[x] += 1
-       end.reject do |k, v|
-         k.nil? || v < 2
-       end
-       dupes.empty? ? nil : raise(SmarterCSV::DuplicateHeaders, "Duplicate Headers in CSV: #{dupes.inspect}")
+      header_counts = Hash.new(0)
+      headers.each { |header| header_counts[header] += 1 unless header.nil? }
+
+      duplicates = header_counts.select { |_, count| count > 1 }
+
+      unless duplicates.empty?
+        raise(SmarterCSV::DuplicateHeaders, "Duplicate Headers in CSV: #{duplicates.inspect}")
+      end
     end
 
-    def required_headers(array, required = [])
+    # def unique_headers_old(array)
+    #   dupes = headers.each_with_object({}) do |x, h|
+    #      # when we remove fields we map them to nil - we don't count these as dupes
+    #      h[x] ||= 0
+    #      h[x] += 1
+    #    end.reject do |k, v|
+    #      k.nil? || v < 2
+    #    end
+    #    dupes.empty? ? nil : raise(SmarterCSV::DuplicateHeaders, "ERROR: duplicate headers: #{dupes.inspect}")
+    # end
+
+    def required_headers(headers, required = [])
       raise(SmarterCSV::IncorrectOption, "ERROR: required_headers validation needs an array argument") unless required.is_a?(Array)
 
-      missing = required.each_with_object([]){ |x, a| a << x unless headers.include?(x) }
-      missing.empty? ? nil : raise(SmarterCSV::MissingHeaders, "Missing Headers in CSV: #{missing.inspect}")
+      headers_set = headers.to_set
+      missing = required.select { |r| !headers_set.include?(r) }
+
+      unless missing.empty?
+        raise(SmarterCSV::MissingHeaders, "Missing Headers in CSV: #{missing.inspect}")
+      end
     end
+
+    # def required_headers_old(array, required = [])
+    #   raise(SmarterCSV::IncorrectOption, "ERROR: required_headers validation needs an array argument") unless required.is_a?(Array)
+
+    #   missing = required.each_with_object([]){ |x, a| a << x unless headers.include?(x) }
+    #   missing.empty? ? nil : raise(SmarterCSV::MissingHeaders, "ERROR: missing headers: #{missing.inspect}")
+    # end
   end
 end
