6f9ba69a2a554d78aae9d0fb233eec3d781544be
Tilo Sloboda <tilo.sloboda@gmail.com>
2023-12-31 08:29:51 +0100

    small refactor

--- lib/smarter_csv/header_validations.rb
+++ lib/smarter_csv/header_validations.rb
@@ -13,6 +13,11 @@ module SmarterCSV
     # ---- V1.x Version: validate the headers -----------------------------------------------------------------
 
     def header_validations_v1(headers, options)
+      check_duplicate_headers_v1(headers, options)
+      check_required_headers_v1(headers, options)
+    end
+
+    def check_duplicate_headers_v1(headers, options)
       duplicate_headers = []
       headers.compact.each do |k|
         duplicate_headers << k if headers.select{|x| x == k}.size > 1
@@ -21,7 +26,9 @@ module SmarterCSV
       unless duplicate_headers.empty?
         raise SmarterCSV::DuplicateHeaders, "ERROR: duplicate headers: #{duplicate_headers.join(',')}"
       end
+    end
 
+    def check_required_headers_v1(headers, options)
       if options[:required_keys] && options[:required_keys].is_a?(Array)
         missing_keys = []
         options[:required_keys].each do |k|
@@ -60,32 +67,22 @@ module SmarterCSV
     #
     # the computed options can be accessed via @options
 
-    @unique_headers = nil
-
     def unique_headers(array)
-      @unique_headers ||= proc {|headers|
-        dupes = headers.each_with_object({}) do |x, h|
-          # when we remove fields we map them to nil - we don't count these as dupes
-          h[x] ||= 0
-          h[x] += 1
-        end.reject do |k, v|
-          k.nil? || v < 2
-        end
-        dupes.empty? ? nil : raise(SmarterCSV::DuplicateHeaders, "Duplicate Headers in CSV: #{dupes.inspect}")
-      }
-      @unique_headers.call(array)
+      dupes = headers.each_with_object({}) do |x, h|
+         # when we remove fields we map them to nil - we don't count these as dupes
+         h[x] ||= 0
+         h[x] += 1
+       end.reject do |k, v|
+         k.nil? || v < 2
+       end
+       dupes.empty? ? nil : raise(SmarterCSV::DuplicateHeaders, "Duplicate Headers in CSV: #{dupes.inspect}")
     end
 
-    @required_headers = nil
-
     def required_headers(array, required = [])
-      @required_headers ||= proc {|headers, required = []|
-        raise(SmarterCSV::IncorrectOption, "ERROR: required_headers validation needs an array argument") unless required.is_a?(Array)
+      raise(SmarterCSV::IncorrectOption, "ERROR: required_headers validation needs an array argument") unless required.is_a?(Array)
 
-        missing = required.each_with_object([]){ |x, a| a << x unless headers.include?(x) }
-        missing.empty? ? nil : raise(SmarterCSV::MissingHeaders, "Missing Headers in CSV: #{missing.inspect}")
-      }
-      @required_headers.call(array, required)
+      missing = required.each_with_object([]){ |x, a| a << x unless headers.include?(x) }
+      missing.empty? ? nil : raise(SmarterCSV::MissingHeaders, "Missing Headers in CSV: #{missing.inspect}")
     end
   end
 end
