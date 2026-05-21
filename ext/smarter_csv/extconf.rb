ee729611b35b2d7c4262ae243148a373960aea0d
Tilo Sloboda <tilo.sloboda@gmail.com>
2026-05-21 08:30:36 -0700

    do not compile extension on non-MRI Rubies

--- ext/smarter_csv/extconf.rb
+++ ext/smarter_csv/extconf.rb
@@ -3,6 +3,15 @@
 require 'mkmf'
 require "rbconfig"
 
+# On non-MRI Rubies (JRuby, TruffleRuby, ...) there is no C extension to build, and trying to build
+# it breaks `gem install` for anything that depends on smarter_csv. Write a no-op Makefile so install
+# succeeds, then stop. At runtime SmarterCSV falls back to its pure-Ruby parser (it checks whether the
+# C functions actually loaded via respond_to?(:parse_csv_line_c)).
+unless RUBY_ENGINE == 'ruby'
+  File.write('Makefile', dummy_makefile($srcdir).join)
+  exit 0
+end
+
 if RbConfig::MAKEFILE_CONFIG["CFLAGS"].include?("-g -O3")
   fixed_CFLAGS = RbConfig::MAKEFILE_CONFIG["CFLAGS"].sub("-g -O3", "-O3 $(cflags)")
   puts("Fix CFLAGS: #{RbConfig::MAKEFILE_CONFIG["CFLAGS"]} -> #{fixed_CFLAGS}")
